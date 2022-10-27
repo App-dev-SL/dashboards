2ee0b0132d2d70fb74f55c77a59a40abbed71747
Summer-Lu0208 <slu12@chicagobooth.edu>
2022-10-27 20:44:45 +0000

    final final

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -18,4 +18,29 @@ class  CurrenciesController < ApplicationController
       render({:template =>"currency_templates/step_two.html.erb"})
   end
 
+  def translation
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+
+  
+
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+   
+    @raw_convert_data = open("https://api.exchangerate.host/convert?from=" + @from_symbol.to_s + "&to=" + @to_symbol.to_s).read
+    @parsed_convert_data = JSON.parse(@raw_convert_data)
+    @convert_info = @parsed_convert_data.fetch("info")
+    @convert_rate = @convert_info.fetch("rate")
+
+
+   
+    render ({:template =>"currency_templates/step_three.html.erb"})
+
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ get("/forex", {:controller =>"currencies", :action =>"first_currency"})
 
 get("/forex/:from_currency", {:controller =>"currencies", :action =>"second_currency"})
 
-
+get("/forex/:from_currency/:to_currency", {:controller =>"currencies", :action =>"translation"})
 
 
 end
